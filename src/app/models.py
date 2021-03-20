cae532c4d5d482b7d4bfaedeee24e39fa4d7c0ff
aminul91 <sumon2907@gmail.com>
2021-03-20 11:57:07 +0100

    error resolved

--- src/app/models.py
+++ src/app/models.py
@@ -32,9 +32,9 @@ class tutorials_paths(models.Model):
     links_name = models.CharField(max_length=70,blank=True)
     links_path = models.CharField(max_length=300,blank=True)
     links_val = models.IntegerField(default=0,primary_key = True)
-    type_value = models.ForeignKey(tutorial_type, on_delete=models.CASCADE)
+    type_value = models.ForeignKey(tutorial_types, on_delete=models.CASCADE)
     categ_name = models.CharField(max_length=100,blank=True)
-    language_value = models.ForeignKey(language_type, on_delete=models.CASCADE)
+    language_value = models.ForeignKey(language_types, on_delete=models.CASCADE)
     language_type = models.CharField(max_length=100,blank=True)
     def __str__(self):
         return self.links_path
